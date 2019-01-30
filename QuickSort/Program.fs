01b72a031e88b34e5126e21b45570465c0d763f4
ChiefTwoPencils <robertwilk916@gmail.com>
2019-01-29 17:59:43 -0800

    Added parallel imps.

--- QuickSort/Program.fs
+++ QuickSort/Program.fs
@@ -2,6 +2,7 @@
 // See the 'F# Tutorial' project for more help.
 open System
 open System.Diagnostics
+open System.Threading.Tasks
 
 let rec quickSortSequential aList =
     match aList with
@@ -11,6 +12,31 @@ let rec quickSortSequential aList =
             List.partition (fun n -> n < first) rest
         quickSortSequential smaller @ (first :: quickSortSequential larger)
 
+let rec quickSortParallel aList =
+    match aList with
+    | [] -> []
+    | first :: rest ->
+        let smaller, larger =
+            List.partition (fun n -> n < first) rest
+        let left = Task.Run(fun () -> quickSortParallel smaller)
+        let right = Task.Run(fun () -> quickSortParallel larger)
+        left.Result @ (first :: right.Result)
+
+let rec quickSortParallelWithDepth depth aList =
+    match aList with
+    | [] -> []
+    | first :: rest ->
+        let smaller, larger =
+            List.partition (fun n -> n < first) rest
+        if depth < 0 then
+            let left = quickSortParallelWithDepth depth smaller
+            let right = quickSortParallelWithDepth depth larger
+            left @ (first :: right)
+        else
+            let left = Task.Run(fun () -> quickSortParallelWithDepth (depth - 1) smaller)
+            let right = Task.Run(fun () -> quickSortParallelWithDepth (depth - 1) larger)
+            left.Result @ (first :: right.Result)
+
 let getRandomArray() =
     let rand = Random()
     List.init 1000000 (fun __ -> rand.Next())
@@ -19,7 +45,7 @@ let getRandomArray() =
 let main _ = 
     let stopWatch = Stopwatch.StartNew()
     let aList = getRandomArray()
-    let sorted = aList |> quickSortSequential
+    let sorted = aList |> quickSortParallelWithDepth 8 // quickSortParallel // quickSortSequential
     stopWatch.Stop |> ignore
     Console.WriteLine("{0} elements : {1}", sorted.Length, (float) stopWatch.ElapsedMilliseconds / 1000.0) |> ignore
     0 // return an integer exit code
