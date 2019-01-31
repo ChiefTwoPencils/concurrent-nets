50343da983e6c79a0ffb5a145101708e86720919
ChiefTwoPencils <robertwilk916@gmail.com>
2019-01-30 19:24:32 -0800

    Added compose and simple example.

--- Snippets/Program.cs
+++ Snippets/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Snippets
 {
@@ -10,6 +7,29 @@ namespace Snippets
     {
         static void Main(string[] args)
         {
+            Console.WriteLine((int) '0');
+            Func<int, string> f = i =>
+            {
+                var j = i % 10;
+                var collection = Enumerable
+                    .Range(0, 10)
+                    .Select(k => '0' + (char)k)
+                    .ToList()
+                    .ElementAt(j)
+                    .ToString();
+                return collection;
+            };
+
+            Func<string, int> g = s => int.Parse(s);
+
+            var gof = f.Compose(g);
+            Console.WriteLine(gof(110));
         }
+    }
+
+    public static class Exts
+    {
+        public static Func<T1, T3> Compose<T1, T2, T3>(this Func<T1, T2> f, Func<T2, T3> g)
+            => x => g(f(x));
     }
 }
