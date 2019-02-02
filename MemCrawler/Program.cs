9a7ce3975b2287d34c7d692a3d04d3f25fab8623
ChiefTwoPencils <robertwilk916@gmail.com>
2019-02-01 16:23:49 -0800

    Added mem-ed crawler proj.

---
+++ MemCrawler/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MemCrawler.WebCrawlerNorm;
+
+namespace MemCrawler
+{
+    class Program
+    {
+        /// <summary>
+        /// Adds a cache to a func.
+        /// </summary>
+        /// <typeparam name="T">In type.</typeparam>
+        /// <typeparam name="R">Return type.</typeparam>
+        /// <param name="f">Function to memoize.</param>
+        /// <returns>A memoized function.</returns>
+        static Func<T, R> Memoize<T, R>(Func<T, R> f)
+        where T : IComparable
+        {
+            var cache = new Dictionary<T, R>();
+            return key => { 
+                if (cache.ContainsKey(key))
+                {
+                    return cache[key];
+                }
+                return (cache[key] = f(key));
+            };
+        }
+
+        /// <summary>
+        /// Adds a cache to a func.
+        /// </summary>
+        /// <typeparam name="T">In type.</typeparam>
+        /// <typeparam name="R">Return type.</typeparam>
+        /// <param name="f">Function to memoize.</param>
+        /// <returns>A memoized function.</returns>
+        static Func<T, R> MemoizeThreadSafe<T, R>(Func<T, R> f)
+        where T : IComparable
+        {
+            var cache = new ConcurrentDictionary<T, R>();
+            return key => cache.GetOrAdd(key, k => f(k));
+        }
+
+        static void Main()
+        {
+            var urls = new List<string>
+            {
+                @"http://www.google.com",
+                @"http://www.microsoft.com",
+                @"http://www.bing.com",
+                @"http://www.google.com"
+            };
+            var watch = new Stopwatch();
+            Console.WriteLine($"Starting basic crawl");
+            watch.Start();
+            Test(urls, WebCrawler);
+            watch.Stop();
+            Console.WriteLine($"Finshed basic in {watch.ElapsedMilliseconds / 1000}.\n\n");
+            Console.WriteLine($"Starting mem-ed crawl");
+            watch.Reset();
+            watch.Start();
+            Test(urls, MemoizeThreadSafe<string, IEnumerable<string>>(WebCrawler));
+            watch.Stop();
+            Console.WriteLine($"Finshed mem-ed in {watch.ElapsedMilliseconds / 1000}");
+        }
+
+        static void Test(List<string> urls, Func<string, IEnumerable<string>> func)
+        {
+            urls.AsParallel()
+                .SelectMany(url => func(url))
+                .Select(page => ExtractWebPageTitle(page))
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ MemCrawler/WebCrawlerNorm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MemCrawler
+{
+    class WebCrawlerNorm
+    {
+        public static IEnumerable<string> WebCrawler(string url)
+        {
+            var content = GetWebContent(url);
+            yield return content;
+            foreach (var item in AnalyzeHtmlContent(content))
+            {
+                yield return GetWebContent(item);
+            }
+        }
+
+        public static string GetWebContent(string url)
+        {
+            try
+            {
+                using (var wc = new WebClient())
+                {
+                    return wc.DownloadString(new Uri(url));
+                }
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+        public static IEnumerable<string> AnalyzeHtmlContent(string text)
+        {
+            foreach(var url in regexLink.Matches(text))
+            {
+                yield return url.ToString();
+            }
+        }
+
+        public static string ExtractWebPageTitle(string pageText)
+        {
+            if (regexTitle.IsMatch(pageText))
+            {
+                return regexTitle.Match(pageText).Groups["title"].Value;
+            }
+            return "No Page Title Found!";
+        }
+
+        public static readonly Regex regexLink =
+            new Regex(@"(?<=href=('|""))https?://.*?(?=\1)");
+
+        public static readonly Regex regexTitle =
+            new Regex("<title>(?<title>.*?)<\\/title>", RegexOptions.Compiled);
+    }
+}
