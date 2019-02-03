207700d2085cfc5c791e8557c3739c618fd37e31
ChiefTwoPencils <robertwilk916@gmail.com>
2019-02-03 15:31:08 -0800

    Added thread-safe Singleton.

---
+++ MemCrawler/Singleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MemCrawler
+{
+    public sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> lazy =
+            new Lazy<Singleton>(() => new Singleton(), true);
+
+        public static Singleton Instance => lazy.Value;
+
+        private Singleton() { }
+    }
+}
