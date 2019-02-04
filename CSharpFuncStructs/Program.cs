76fd7980f5f422195355768b59950f43684cd287
ChiefTwoPencils <robertwilk916@gmail.com>
2019-02-03 19:43:26 -0800

    Added projs for func structs (ch. 3).

---
+++ CSharpFuncStructs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFuncStructs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
