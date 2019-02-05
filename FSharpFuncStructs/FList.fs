d10314eee307f0114cbb946915dc4c5326e9cf01
ChiefTwoPencils <robertwilk916@gmail.com>
2019-02-04 20:35:58 -0800

    Add fs FList.

---
+++ FSharpFuncStructs/FList.fs
@@ -0,0 +1,18 @@
+﻿module FList
+
+type FList<'a> =
+    | Empty
+    | Cons of head: 'a * tail: FList<'a>
+
+let rec map f (list: FList<'a>) =
+    match list with
+    | Empty -> Empty
+    | Cons(head, tail) -> Cons(f head, map f tail)
+
+let rec filter p (list: FList<'a>) =
+    match list with
+    | Empty -> Empty
+    | Cons(head, tail) when p head -> Cons(head, filter p tail)
+    | Cons(_, tail) -> filter p tail
+
+
