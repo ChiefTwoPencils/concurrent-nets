be108138999c2fd12dd67fbd463d58fe10ca6dac
ChiefTwoPencils <robertwilk916@gmail.com>
2019-02-04 20:03:09 -0800

    Added equivalent F# immutable Person.

---
+++ FSharpFuncStructs/Person.fs
@@ -0,0 +1,9 @@
+﻿module Person
+
+type Person = { FirstName:string; LastName:string; Age:int }
+let person = { FirstName = "Tommy"; LastName = "Barone"; Age = 22 }
+// person.Age <- 66 => ERROR!
+let newPerson = { person with FirstName = "Jewels" }
+
+let mutable badPerson = { FirstName = "Evil"; LastName = "Bastard"; Age = 666 }
+badPerson = { badPerson with FirstName = "Superevil" } |> ignore
--- FSharpFuncStructs/Program.fs
+++ FSharpFuncStructs/Program.fs
@@ -1,7 +1,20 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open System
+open Person
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let person = { FirstName = "Tommy"; LastName = "Barone"; Age = 22 }
+    Console.WriteLine(sprintf "%A" person)
+    // person.Age <- 66 => ERROR!
+    let newPerson = { person with FirstName = "Jewels" }
+    Console.WriteLine(sprintf "%A" person)
+    Console.WriteLine(sprintf "%A" newPerson)
+
+    let mutable badPerson = { FirstName = "Evil"; LastName = "Bastard"; Age = 666 }
+    Console.WriteLine(sprintf "%A" badPerson)
+    badPerson <- { badPerson with FirstName = "Superevil" }
+    Console.WriteLine(sprintf "%A" badPerson)
     0 // return an integer exit code
