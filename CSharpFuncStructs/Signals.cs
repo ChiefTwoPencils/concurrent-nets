ffabf8dfd9d597d24daaa5c17d37b6c065692706
ChiefTwoPencils <robertwilk916@gmail.com>
2019-02-04 17:29:44 -0800

    Added conc. coll. example with SignalR hub.

---
+++ CSharpFuncStructs/Signals.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using System.Collections.Concurrent;
+
+namespace CSharpFuncStructs
+{
+    class Signals : Hub
+    {
+        private static ConcurrentDictionary<Guid, string> onlineUsers =
+            new ConcurrentDictionary<Guid, string>();
+
+        public override Task OnConnected()
+        {
+            var connId = new Guid(Context.ConnectionId);
+            var user = Context.User;
+
+            var name = user.Identity.Name;
+            if (onlineUsers.TryAdd(connId, name))
+            {
+                RegisterUserConnection(connId, name);
+            }
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var connId = new Guid(Context.ConnectionId);
+            string name;
+            if (onlineUsers.TryRemove(connId, out name))
+            {
+                DeregisterUserConnection(connId, name);
+            }
+            return base.OnDisconnected(stopCalled);
+        }
+
+        private void RegisterUserConnection(Guid connId, string name) { }
+        private void DeregisterUserConnection(Guid connId, string name) { }
+    }
+}
