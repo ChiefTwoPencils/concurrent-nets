5605e2b7e991c1ffd1d7abb9d544c5ed8880f3b3
ChiefTwoPencils <robertwilk916@gmail.com>
2019-02-04 20:49:50 -0800

    Added cs FList.

---
+++ CSharpFuncStructs/FList.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharpFuncStructs
+{
+    sealed class FList<T>
+    {
+        public T Head { get; }
+        public FList<T> Tail { get; }
+        public bool IsEmpty { get; }
+
+        private FList(T head, FList<T> tail)
+        {
+            Head = head;
+            Tail = tail.IsEmpty
+                ? Empty
+                : tail;
+            IsEmpty = false;
+        }
+
+        private FList()
+        {
+            IsEmpty = true;
+        }
+
+        public static FList<T> Cons(T head, FList<T> tail)
+            => tail.IsEmpty
+                ? new FList<T>(head, Empty)
+                : new FList<T>(head, tail);
+
+        public FList<T> Cons(T head) => new FList<T>(head, this); 
+
+        public static readonly FList<T> Empty = new FList<T>();
+    }
+}
