dacf692938941eb82ebeaf1b89b45d14af6e76f3
ChiefTwoPencils <robertwilk916@gmail.com>
2019-02-06 20:46:54 -0800

    Added Tree.fs.

---
+++ FSharpFuncStructs/Tree.fs
@@ -0,0 +1,30 @@
+﻿module Tree
+
+type Tree<'a> =
+    | Node of leaf: 'a * left: Tree<'a> * right: Tree<'a>
+    | Empty
+
+let rec contains item tree =
+    match tree with
+    | Node(leaf, left, right) -> 
+        if leaf = item then true
+        elif leaf < item then contains item left
+        else contains item right
+    | Empty -> false
+
+let rec insert item tree =
+    match tree with
+    | Node(leaf, left, right) as node ->
+        if leaf = item then node
+        elif leaf < item then Node(leaf, insert item left, right)
+        else Node(leaf, left, insert item right)
+    | Empty -> Node(item, Empty, Empty)
+
+let rec inorder action tree =
+    match tree with
+    | Node(leaf, left, right) ->
+        inorder action left
+        action leaf
+        inorder action right
+    | Empty -> ()
+
