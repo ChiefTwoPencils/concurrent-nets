5202ac696ff2dca78becef4127429c01af2a2b07
ChiefTwoPencils <robertwilk916@gmail.com>
2019-02-06 17:39:15 -0800

    Added fs lazy list.

---
+++ FSharpFuncStructs/LazyList.fs
@@ -0,0 +1,18 @@
+﻿module LazyList
+
+type LazyList<'a> =
+    | Cons of head: 'a * tail: Lazy<'a LazyList>
+    | Empty
+
+let rec append items list =
+    match items with
+    | Cons(head, Lazy(tail)) ->
+        Cons(head, lazy(append tail list))
+    | Empty -> list
+
+let rec iter action list =
+    match list with
+    | Cons(head, Lazy(tail)) ->
+        action head
+        iter action tail
+    | Empty -> ()
