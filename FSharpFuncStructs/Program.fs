edb094abd7a06727516c706cdfb8bde08dd533c0
ChiefTwoPencils <robertwilk916@gmail.com>
2019-02-07 17:37:01 -0800

    Add factorial in continuation-passing style.

--- FSharpFuncStructs/Program.fs
+++ FSharpFuncStructs/Program.fs
@@ -4,6 +4,10 @@
 open System
 open Person
 
+let rec factorialCPS x continuation =
+    if x <= 1 then continuation()
+    else factorialCPS (x - 1) (fun () -> x * continuation())
+
 [<EntryPoint>]
 let main argv = 
     let person = { FirstName = "Tommy"; LastName = "Barone"; Age = 22 }
@@ -17,4 +21,7 @@ let main argv =
     Console.WriteLine(sprintf "%A" badPerson)
     badPerson <- { badPerson with FirstName = "Superevil" }
     Console.WriteLine(sprintf "%A" badPerson)
+
+
+    Console.WriteLine(sprintf "Continuation factorial with 4: %d" (factorialCPS 4 (fun () -> 1)))
     0 // return an integer exit code
