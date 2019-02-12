a49c7e8ddfe69f1c4a6043ca576e75c3ecf1f891
ChiefTwoPencils <robertwilk916@gmail.com>
2019-02-11 18:03:51 -0800

    Added parallelism sln folder + cs Complex #

---
+++ Parallelization/Complex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Parallelization
+{
+    class Complex
+    {
+        public Complex(float real, float imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public float Imaginary { get; }
+        public float Real { get; }
+
+        public float Magnitude
+            => (float) Math.Sqrt(Math.Pow(Real, 2.0) + Math.Pow(Imaginary, 2.0));
+
+        public static Complex operator + (Complex l, Complex r)
+            => new Complex(l.Real + r.Real, l.Imaginary + r.Imaginary);
+
+        public static Complex operator * (Complex l, Complex r)
+            => new Complex(l.Real * r.Real - l.Imaginary * r.Imaginary,
+                l.Real * r.Imaginary + l.Imaginary * r.Real);
+    }
+}
